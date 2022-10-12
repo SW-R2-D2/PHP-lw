32dea38d8740d51a44d3a75ec0004beb7b4c7de8
Alexander <79600959910@ya.ru>
2022-10-13 00:33:09 +0300

    The practical lw0 commit. Changed line 59 echo calculator('5+5+5+5+5');

---
+++ lw0.php
@@ -0,0 +1,59 @@
+<?php
+function calculator(string $value): string
+{
+    $signArr = [];
+    $numberArr = [];
+    $numbers = '0123456789';
+    $str = '';
+
+    foreach (str_split($value) as $char) {
+        if (strpos($numbers, $char)) {
+            $str = $char;
+            $numberArr[] = intval($str);
+            $str = '';
+        } else {
+            $signArr[] = $char;
+        }
+    }
+
+    $sum = 0;
+    foreach ($signArr as $key => $sign) {
+        if ($key === 0) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key] + $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum += $numberArr[$key] - $numberArr[$key + 1];
+            }
+        }
+        if ($key === 1) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+        if ($key === 2) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+        if ($key === 3) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+    }
+    if ($sum < 0) {
+        $sum = 0;
+    }
+    return $sum;
+}
+echo calculator('5-5+5+5+5');
