288d4924e27046092bf59f23aa476749e9a7010a
Alexander <79600959910@ya.ru>
2022-10-29 22:09:45 +0300

    Completed task 2.

---
+++ lw0 (Task 1, 2).php
@@ -0,0 +1,86 @@
+<?php
+//Task 1.
+function calculator(string $value): string
+{
+    $signArr = [];
+    $numberArr = [];
+    $numbers = '0123456789';
+    $str = '';
+
+    foreach (str_split($value) as $char) {
+        if (strpos($numbers, $char)) {
+            $str = $char;
+            $numberArr[] = intval($str);
+            $str = '';
+        } else {
+            $signArr[] = $char;
+        }
+    }
+
+    $sum = 0;
+    foreach ($signArr as $key => $sign) {
+        if ($key === 0) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key] + $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum += $numberArr[$key] - $numberArr[$key + 1];
+            }
+        }
+        if ($key === 1) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+        if ($key === 2) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+        if ($key === 3) {
+            if ($sign === '+') {
+                $sum += $numberArr[$key + 1];
+            }
+            if ($sign === '-') {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+    }
+    return $sum;
+}
+echo calculator('5-5+5+5-5');
+?>
+
+<?php
+//Task 2.
+function sumTime($time1, $time2)
+{
+    $seconds = 0;
+
+    $total = [$time1, $time2];
+
+    foreach ($total as $time) {
+        list($hour, $minute, $second) = explode(':', $time);
+        $seconds += $hour * 3600;
+        $seconds += $minute * 60;
+        $seconds += $second;
+    }
+    $hours = floor($seconds / 3600);
+    $seconds -= $hours * 3600;
+    $minutes = floor($seconds / 60);
+    $seconds -= $minutes * 60;
+
+    if ($hours > 24)
+        $hours -= 24;
+
+    return sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+}
+
+echo sumTime('10:00:00', '23:31:00');
+?>
